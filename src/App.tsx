eb14281bf44d723fb71fe23b78aabf2549252a8b
KhEsanukaev <halid.esanukaev.86@mail.ru>
2025-04-16 18:16:20 +0300

    feat/сделал новый компонент 'catalogPage', чтобы подкличить в хидере каталог.Также настроил роуты

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,20 @@ import Footer from './components/Footer'
 import Contacts from './components/Contacts'
 import Castomers from './components/Castomers'
 import Reservation from './components/Reservation'
-import CustomDoubleCarousel from './components/DoubleCarousel'
+import CatalogPage from './components/CatalogPage'
 
 function App() {
   return (
     <>
       <Header />
-      <CustomDoubleCarousel/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/contacts" element={<Contacts />} />
         <Route path="/castomers" element={<Castomers />} />
         <Route path="/reservation" element={<Reservation/>} />
+        <Route path="/catalog" element={<CatalogPage />} />
+
       </Routes>
       <Footer/>
     </>
