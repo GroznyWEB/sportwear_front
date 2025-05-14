6b774b0b541ed845738f1cc04e3e061f3591a04c
root <root@bxtptgxelb.local>
2025-05-14 22:59:11 +0000

    исправил ссылку на ватцап

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()], // Только React-плагин
+  server: {
+    host: '0.0.0.0',  // разрешает доступ с любых IP (опционально)
+    allowedHosts: [
+      'subkimonos.ru',  // ваш домен
+      'www.subkimonos.ru',  // с www, если нужно
+    ],
+  },
   build: {
     // Базовые оптимизации билда
     chunkSizeWarningLimit: 1500,
